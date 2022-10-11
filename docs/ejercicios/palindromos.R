8ba10647bac4e884c4ab23b944c8c2e0f993bef3
Pao Corrales <paobcorrales@gmail.com>
2022-10-11 12:09:07 -0300

    build

---
+++ docs/ejercicios/palindromos.R
@@ -0,0 +1,31 @@
+# Extrae el e-nesimo digito de un numero (arrancando desde la izquierda)
+get_digito <- function(num, n) {
+  # remueve los digitos de la izquierda, luego los de la derecha
+  (num %% (10 ^ n)) %/% (10 ^ (n - 1))
+}
+
+# Revisa si un numero positivo es un palindromo
+palindromo <- function(num) {
+  digitos <- floor(log(num, 10)) + 1
+  for (x in 1:((digits %/% 2))) {
+    digito1 <- get_digito(num, x)
+    digito2 <- get_digito(num, (digits + 1) - x)
+    if (digito1 != digito2)
+      return(FALSE)
+  }
+  return(TRUE)
+}
+
+# Encuentra el palindromo mas grande contruido multiplicando 2 numeros de 3 digitos
+palindromo_grande <- function() {
+  mejor <- 0
+  for (x in 100:999) {
+    for (y in x:999) {
+      candidato <- x * y
+      if (candidato > mejor && palindromo(candidato)) {
+        mejor <- candidato
+      }
+    }
+  }
+  mejor
+}
